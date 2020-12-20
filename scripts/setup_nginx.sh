911394e6bb6444ae1733643ac338b1853306ec4a
Tanmay1221 <tanmayfuse16@gmail.com>
2020-12-20 20:22:14 +0530

    some changes

--- scripts/setup_nginx.sh
+++ scripts/setup_nginx.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+cd ~
 sudo cp /var/www/nginx_config/default /etc/nginx/sites-available/
+cd ~
 sudo service nginx restart
+cd ~
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+cd ~
 sudo pm2 stop all
 sudo pm2 start /var/www/index.js
+cd ~
