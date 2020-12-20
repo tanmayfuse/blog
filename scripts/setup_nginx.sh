e760b5a5d5269806511f031a30ce98cfbdc7dc37
Tanmay1221 <tanmayfuse16@gmail.com>
2020-12-20 19:50:59 +0530

    change scripts

--- scripts/setup_nginx.sh
+++ scripts/setup_nginx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo cp /nginx_config/default /etc/nginx/sites-available/default
+sudo cp /var/www/nginx_config/default /etc/nginx/sites-available/
 sudo service nginx restart
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo pm2 stop all
-sudo pm2 start /demo1/index.js
+sudo pm2 start /var/www/index.js
